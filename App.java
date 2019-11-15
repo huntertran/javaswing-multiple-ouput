c5e46de7a10d15c37313c79798045de60385a72b
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-11-15 15:08:18 -0500

    2 window at the same time

--- App.java
+++ App.java
@@ -6,21 +6,29 @@ import javax.swing.JFrame;
 public class App {
 
     public static void main(String[] args) {
+        WindowPane testOutput1 = createWindowPane("Test", 600, 600);
+        WindowPane testOutput2 = createWindowPane("Test2", 600, 600);
+
+        PrintStream standard = System.out;
+        PrintStream newPrintStream = new PrintStream(new WindowOutputStream(testOutput1));
+        PrintStream newPrintStream2 = new PrintStream(new WindowOutputStream(testOutput2));
+
+        printToNewWindow(standard, "Hello world");
+        printToNewWindow(newPrintStream, "Hello world in new window");
+        printToNewWindow(newPrintStream2, "Hello world in another new window");
+    }
+
+    private static WindowPane createWindowPane(String title, int width, int height) {
         WindowPane windowPane = new WindowPane();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.add(windowPane);
-        frame.setSize(600, 600);
+        frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.setTitle("Test");
-
-        PrintStream standard = System.out;
-        PrintStream newPrintStream = new PrintStream(new WindowOutputStream(windowPane));
-
-        printToNewWindow(standard, "Hello world");
-        printToNewWindow(newPrintStream, "Hello world in new window");
+        frame.setTitle(title);
+        return windowPane;
     }
 
     public static void printToNewWindow(PrintStream printStream, String content) {
