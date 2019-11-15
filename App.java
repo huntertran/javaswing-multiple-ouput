e79eec4b590b9e039ceb2e5ee15eba44b659cfeb
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2019-11-15 15:05:53 -0500

    even more simplified, with custom font

--- App.java
+++ App.java
@@ -2,33 +2,29 @@ import java.io.PrintStream;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class App {
 
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException ex) {
-        } catch (InstantiationException ex) {
-        } catch (IllegalAccessException ex) {
-        } catch (UnsupportedLookAndFeelException ex) {
-        }
-
-        CapturePane capturePane = new CapturePane();
+        WindowPane windowPane = new WindowPane();
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        frame.add(capturePane);
-        frame.setSize(200, 200);
+        frame.add(windowPane);
+        frame.setSize(600, 600);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setTitle("Test");
 
-        PrintStream ps = System.out;
-        System.setOut(new PrintStream(new StreamCapturer("STDOUT", capturePane, ps)));
+        PrintStream standard = System.out;
+        PrintStream newPrintStream = new PrintStream(new WindowOutputStream(windowPane));
 
-        System.out.println("Hello, this is a test");
-        System.out.println("Wave if you can see me");
+        printToNewWindow(standard, "Hello world");
+        printToNewWindow(newPrintStream, "Hello world in new window");
+    }
+
+    public static void printToNewWindow(PrintStream printStream, String content) {
+        System.setOut(printStream);
+        System.out.println(content);
     }
 }
--- WindowPane.java
+++ WindowPane.java
@@ -1,25 +1,40 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-public class CapturePane extends JPanel {
-
-    /**
-     *
-     */
+public class WindowPane extends JPanel {
     private static final long serialVersionUID = 3555490927568246300L;
     private JTextArea output;
 
-    public CapturePane() {
+    public WindowPane() {
         setLayout(new BorderLayout());
         output = new JTextArea();
+        output.setBackground(Color.BLACK);
+        output.setForeground(Color.WHITE);
+        Font font = loadFont();
+        output.setFont(font);
         add(new JScrollPane(output));
     }
 
-    // @Override
+    public Font loadFont() {
+        Font font = null;
+        try {
+            Font localFont = Font.createFont(Font.TRUETYPE_FONT,
+                                             App.class.getResourceAsStream("RobotoMono-Regular.ttf"));
+
+            font = localFont.deriveFont(Font.PLAIN, 14);
+        } catch (Exception ex) {
+            font = null;
+        }
+
+        return font;
+    }
+
     public void appendText(final String text) {
         if (EventQueue.isDispatchThread()) {
             output.append(text);
@@ -31,7 +46,6 @@ public class CapturePane extends JPanel {
                     appendText(text);
                 }
             });
-
         }
     }
 }
